11810e76595f194a56397d1a4af984ca3aa8eef8
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-06-28 15:56:43 +0300

    push

--- src/script.js
+++ src/script.js
@@ -1,15 +1,40 @@
-// Функція для отримання всіх студентів
-function getStudents() {
- // твій код
 
+
+
+// Функція для отримання всіх студентів
+
+function getStudents() {
+return fetch("http://localhost:3000/students")
+  .then((res) => res.json()).then(data => renderStudents(data))
  }
+console.log(getStudents())
+
 
 
 // Функція для відображення студентів у таблиці
 function renderStudents(students) {
- // твій код
-
+return students.map((object) => {
+    const skillsArr = object.skills
+    const skillsStr = skillsArr.toString()
+      const newObject = `
+          <tr>
+            <th>${object.id}/th>
+            <th${object.name}</th>
+            <th>${object.age}</th>
+            <th>${object.cours}</th>
+            <th>${skillsStr}</th>
+            <th>${object.email}</th>
+            <th>${object.isEnrolled}</th>
+            <th><button type="button" id="delete">Видалити</button></th>
+            <th><button type="button" id="update">Оновити</button></th>
+          </tr>
+      
+        `;
+      return newObject;
+    }).join("");
 }
+
+
 
 
 // Функція для додавання нового студента
