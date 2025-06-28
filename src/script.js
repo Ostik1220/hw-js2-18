0e89a0e358cf70fe00a2c6afbdaca2464a404344
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-06-28 22:09:02 +0300

    commit

--- src/script.js
+++ src/script.js
@@ -1,37 +1,33 @@
-
-
-
-// Функція для отримання всіх студентів
-
 function getStudents() {
-return fetch("http://localhost:3000/students")
-  .then((res) => res.json()).then(data => renderStudents(data))
- }
-console.log(getStudents())
+  return fetch("http://localhost:3000/students")
+    .then((res) => res.json())
+    .then((data) => {
+      const tableBody = document.querySelector("tbody");
+      tableBody.innerHTML = renderStudents(data).join('');
+    })
+}
+
+getStudents(); 
 
 
-
-// Функція для відображення студентів у таблиці
 function renderStudents(students) {
-return students.map((object) => {
-    const skillsArr = object.skills
-    const skillsStr = skillsArr.toString()
-      const newObject = `
-          <tr>
-            <th>${object.id}/th>
-            <th${object.name}</th>
-            <th>${object.age}</th>
-            <th>${object.cours}</th>
-            <th>${skillsStr}</th>
-            <th>${object.email}</th>
-            <th>${object.isEnrolled}</th>
-            <th><button type="button" id="delete">Видалити</button></th>
-            <th><button type="button" id="update">Оновити</button></th>
-          </tr>
-      
-        `;
-      return newObject;
-    }).join("");
+  return students.map((object) => {
+    const skillsArr = object.skills;
+    const skillsStr = skillsArr.join(', '); 
+    return `
+      <tr>
+        <td>${object.id}</td>
+        <td>${object.name}</td>
+        <td>${object.age}</td>
+        <td>${object.course}</td>
+        <td>${skillsStr}</td>
+        <td>${object.email}</td>
+        <td>${object.isEnrolled}</td>
+        <td><button type="button" class="delete-btn">Видалити</button></td>
+        <td><button type="button" class="update-btn">Оновити</button></td>
+      </tr>
+    `;
+  });
 }
 
 
